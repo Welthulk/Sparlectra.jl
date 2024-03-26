0459e1b98a9543e7958415052ecbaf549665f44a
welthulk <g1403.2019@udoschmitz.de>
2024-03-26 21:14:37 +0100

    added lines out of service to results

--- src/results.jl
+++ src/results.jl
@@ -11,70 +11,68 @@ function format_version(version::VersionNumber)
 end
 
 function formatBranchResults(net::ResDataTypes.Net)
-
   formatted_results = @sprintf("\n===========================================================================================================================\n")
 
   formatted_results *= @sprintf("| %-25s | %-5s | %-5s | %-10s | %-10s | %-10s | %-10s | %-10s | %-10s |\n", "Branch", "From", "To", "P [MW]", "Q [MVar]", "P [MW]", "Q [MVar]", "Pv [MW]", "Qv [MVar]")
   formatted_results *= @sprintf("===========================================================================================================================\n")
 
   ∑pv = ∑qv = 0.0
-  ∑pfrom=∑qfrom=∑pto=∑qto=0.0
-  
+  ∑pfrom = ∑qfrom = ∑pto = ∑qto = 0.0
+
   for br in net.branchVec
-    if br.status == 0
-      continue
-    end
     from = br.fromBus
     to = br.toBus
     bName = br.comp.cName
-    pfromVal = (br.fBranchFlow.pFlow === nothing) ? NaN : br.fBranchFlow.pFlow
-    qfromVal = (br.fBranchFlow.qFlow === nothing) ? NaN : br.fBranchFlow.qFlow
+    if br.status == 1
+      pfromVal = (br.fBranchFlow.pFlow === nothing) ? NaN : br.fBranchFlow.pFlow
+      qfromVal = (br.fBranchFlow.qFlow === nothing) ? NaN : br.fBranchFlow.qFlow
 
-    ptoVal = (br.tBranchFlow.pFlow === nothing) ? NaN : br.tBranchFlow.pFlow
-    qtoVal = (br.tBranchFlow.qFlow === nothing) ? NaN : br.tBranchFlow.qFlow
+      ptoVal = (br.tBranchFlow.pFlow === nothing) ? NaN : br.tBranchFlow.pFlow
+      qtoVal = (br.tBranchFlow.qFlow === nothing) ? NaN : br.tBranchFlow.qFlow
 
-    VmFrom = (br.fBranchFlow.vm_pu === nothing) ? NaN : br.fBranchFlow.vm_pu
-    VaFrom = (br.fBranchFlow.va_deg === nothing) ? NaN : br.fBranchFlow.va_deg
+      VmFrom = (br.fBranchFlow.vm_pu === nothing) ? NaN : br.fBranchFlow.vm_pu
+      VaFrom = (br.fBranchFlow.va_deg === nothing) ? NaN : br.fBranchFlow.va_deg
 
-    VmTo = (br.tBranchFlow.vm_pu === nothing) ? NaN : br.tBranchFlow.vm_pu
-    VaTo = (br.tBranchFlow.va_deg === nothing) ? NaN : br.tBranchFlow.va_deg
-    ratedS = isnothing(br.sn_MVA) ? 0.0 : br.sn_MVA
-    ∑pfrom += pfromVal
-    ∑qfrom += qfromVal
-    ∑pto += ptoVal
-    ∑qto += qtoVal
+      VmTo = (br.tBranchFlow.vm_pu === nothing) ? NaN : br.tBranchFlow.vm_pu
+      VaTo = (br.tBranchFlow.va_deg === nothing) ? NaN : br.tBranchFlow.va_deg
+      ratedS = isnothing(br.sn_MVA) ? 0.0 : br.sn_MVA
+      ∑pfrom += pfromVal
+      ∑qfrom += qfromVal
+      ∑pto += ptoVal
+      ∑qto += qtoVal
 
-    v1 = VmFrom * exp(im * deg2rad(VaFrom))
-    v2 = VmTo * exp(im * deg2rad(VaTo))
-    # Losses: abs( Vf / tau - Vt ) ^ 2 / (Rs - j Xs)
-    ratio = (br.ratio != 0.0) ? br.ratio : 1.0
-    angle = (br.ratio != 0.0) ? br.angle : 0.0
-    tap = calcComplexRatio(ratio, angle)
-    
+      v1 = VmFrom * exp(im * deg2rad(VaFrom))
+      v2 = VmTo * exp(im * deg2rad(VaTo))
+      # Losses: abs( Vf / tau - Vt ) ^ 2 / (Rs - j Xs)
+      ratio = (br.ratio != 0.0) ? br.ratio : 1.0
+      angle = (br.ratio != 0.0) ? br.angle : 0.0
+      tap = calcComplexRatio(ratio, angle)
 
-    Vdiff = v1 / tap - v2
+      Vdiff = v1 / tap - v2
 
-    y = inv((br.r_pu + br.x_pu * im)) #+ 0.5*(br.g_pu + br.b_pu*im)
+      y = inv((br.r_pu + br.x_pu * im)) #+ 0.5*(br.g_pu + br.b_pu*im)
 
-    Sdiff = Vdiff * conj(Vdiff * y) * net.baseMVA
-    pv = real(Sdiff)
-    qv = imag(Sdiff)
-    ∑pv += abs(pv)
-    ∑qv += abs(qv)
-    check = false
-    if ratedS > 0.0
-      if max(abs(pfromVal), abs(ptoVal)) > ratedS
-        check = true
+      Sdiff = Vdiff * conj(Vdiff * y) * net.baseMVA
+      pv = real(Sdiff)
+      qv = imag(Sdiff)
+      ∑pv += abs(pv)
+      ∑qv += abs(qv)
+      check = false
+      if ratedS > 0.0
+        if max(abs(pfromVal), abs(ptoVal)) > ratedS
+          check = true
+        end
       end
+
+      if check
+        bName *= " !"
+      end
+    else
+      pfromVal = qfromVal = ptoVal = qtoVal = pv = qv = 0.0 
     end
-    
-    if check
-      bName *= " !"
-    end  
     formatted_results *= @sprintf("| %-25s | %-5s | %-5s | %-10.3f | %-10.3f | %-10.3f | %-10.3f | %-10.3f |  %-10.3f|\n", bName, from, to, pfromVal, qfromVal, ptoVal, qtoVal, abs(pv), abs(qv))
-    
   end
-  formatted_results *= @sprintf("---------------------------------------------------------------------------------------------------------------------------\n")  
+  formatted_results *= @sprintf("---------------------------------------------------------------------------------------------------------------------------\n")
   #formatted_results *= @sprintf("| %-10.3f | %-10.3f | %-10.3f | %-10.3f |\n", ∑pfrom, ∑qfrom, ∑pto, ∑qto)
 
   total_losses = @sprintf("total losses (I^2*Z): P = %10.3f [MW], Q = %10.3f [MVar]\n", ∑pv, ∑qv)
@@ -97,10 +95,9 @@ function printACPFlowResults(net::ResDataTypes.Net, ct::Float64, ite::Int, tol::
   formatted_version = format_version(vers)
   flowResults, totalLosses = formatBranchResults(net)
 
-  @printf(io,"================================================================================\n")
-  @printf(io,"| SPARLECTRA Version %-10s - AC Power Flow Results                        |\n", formatted_version)
-  @printf(io,"================================================================================\n")
-
+  @printf(io, "================================================================================\n")
+  @printf(io, "| SPARLECTRA Version %-10s - AC Power Flow Results                        |\n", formatted_version)
+  @printf(io, "================================================================================\n")
 
   busses = length(net.nodeVec)
   branches = length(net.branchVec)
@@ -130,7 +127,7 @@ function printACPFlowResults(net::ResDataTypes.Net, ct::Float64, ite::Int, tol::
 
   @printf(io, "Date         :%20s\n", current_date)
   @printf(io, "Iterations   :%10d\n", ite)
-  @printf(io, "Tolerance    : %.1e\n", tol) 
+  @printf(io, "Tolerance    : %.1e\n", tol)
   @printf(io, "Converged in :%10f seconds\n", ct)
   @printf(io, "Case         :%15s\n", net.name)
   @printf(io, "BaseMVA      :%10d\n", net.baseMVA)
@@ -151,19 +148,20 @@ function printACPFlowResults(net::ResDataTypes.Net, ct::Float64, ite::Int, tol::
   @printf(io, "==========================================================================================================================================================================\n")
   @printf(io, "| %-5s | %-20s | %-10s | %-10s | %-10s | %-10s | %-10s | %-10s | %-10s | %-10s | %-10s | %-10s | %-5s |\n", "Nr", "Bus", "Vn [kV]", "V [kV]", "V [pu]", "phi [deg]", "Pg [MW]", "Qg [MVar]", "Pl [MW]", "Ql [MVar]", "Ps [MW]", "Qs [MVar]", "Type")
   @printf(io, "==========================================================================================================================================================================\n")
-  
+
   pGS = qGS = pLS = qLS = ""
   tpGS = tqGS = tpLS = tqLS = 0.0
   pShunt_str = qShunt_str = ""
   tpShunt = tqShunt = 0.0
-  for n in nodes    
-    if !isnothing(n._pƩGen) abs(n._pƩGen) > 1e-6
+  for n in nodes
+    if !isnothing(n._pƩGen)
+      abs(n._pƩGen) > 1e-6
       pGS = @sprintf("%10.3f", n._pƩGen)
       tpGS += n._pƩGen
     else
       pGS = ""
-    end 
-    if !isnothing(n._qƩGen) && abs(n._qƩGen) > 1e-6 
+    end
+    if !isnothing(n._qƩGen) && abs(n._qƩGen) > 1e-6
       qGS = @sprintf("%10.3f", n._qƩGen)
       tqGS += n._qƩGen
     else
@@ -176,7 +174,7 @@ function printACPFlowResults(net::ResDataTypes.Net, ct::Float64, ite::Int, tol::
     else
       pLS = ""
     end
-    if !isnothing(n._qƩLoad) && abs(n._qƩLoad) > 1e-6  
+    if !isnothing(n._qƩLoad) && abs(n._qƩLoad) > 1e-6
       qLS = @sprintf("%10.3f", n._qƩLoad)
       tqLS += n._qƩLoad
     else
@@ -194,8 +192,8 @@ function printACPFlowResults(net::ResDataTypes.Net, ct::Float64, ite::Int, tol::
     else
       qShunt_str = ""
     end
-    typeStr = toString(n._nodeType)    
-    v = n.comp.cVN*n._vm_pu
+    typeStr = toString(n._nodeType)
+    v = n.comp.cVN * n._vm_pu
     nodeName = n.comp.cName
     if !isnothing(n._vmin_pu) && !isnothing(n._vmax_pu)
       if n._vm_pu < n._vmin_pu || n._vm_pu > n._vmax_pu
@@ -216,14 +214,13 @@ function printACPFlowResults(net::ResDataTypes.Net, ct::Float64, ite::Int, tol::
   end
 end
 
-
 function convertPVtoPQ!(net::ResDataTypes.Net)
   for n in net.nodeVec
     if n._nodeType == ResDataTypes.PV
       busIdx = n.busIdx
       for p in net.prosumpsVec
-        if p.comp.cFrom_bus == busIdx          
-          setQGenReplacement!(p, n._qƩGen)          
+        if p.comp.cFrom_bus == busIdx
+          setQGenReplacement!(p, n._qƩGen)
         end
       end
     end
