a57b85a2ec707284653e09365c396d567bfa7345
welthulk <g1403.2019@udoschmitz.de>
2024-03-30 15:52:04 +0100

    bugfix missing imports

--- install_packages.jl
+++
@@ -1,9 +0,0 @@
-using Pkg
-
-PACKAGES = ["SparseArrays", "LinearAlgebra", "Printf", "JuliaFormatter", "Logging", "Test"]
-
-for pkg in PACKAGES
-    if !haskey(Pkg.installed(), Symbol(pkg))
-        Pkg.add(pkg)
-    end
-end
