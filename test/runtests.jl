941bd10ed1059677379fb3e658fd002a1378a7fe
welthulk <g1403.2019@udoschmitz.de>
2024-04-11 19:32:56 +0200

    update testnet

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ include("testgrid.jl")
   @test testNetwork() == true
   @test test_NBI_MDO() == true
   @test test_acpflow(0) == true
-#  @test testExportMatpower() == true #does not run, can´t create testfile, add these test manually
-#  @test testImportMatpower() == true 
-#  @test rmTestfiles() == true 
+  #  @test testExportMatpower() == true #does not run, can´t create testfile, add these test manually
+  #  @test testImportMatpower() == true 
+  #  @test rmTestfiles() == true 
 end
