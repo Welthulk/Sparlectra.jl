4afe1e8f0070be4f80eb389633fb01a9bd543aa3
welthulk <g1403.2019@udoschmitz.de>
2024-04-12 11:53:00 +0200

    added instabil - network casefile

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,8 @@ using Logging
 
 global_logger(ConsoleLogger(stderr, Logging.Warn))
 include("testgrid.jl")
-@testset "Sparlectra.jl" begin
-  @test test3BusNet() == true
+@testset "Sparlectra.jl" begin  
   @test testNetwork() == true
   @test test_NBI_MDO() == true
   @test test_acpflow(0) == true
-  #  @test testExportMatpower() == true #does not run, can´t create testfile, add these test manually
-  #  @test testImportMatpower() == true 
-  #  @test rmTestfiles() == true 
 end
