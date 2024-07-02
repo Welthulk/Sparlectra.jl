b9c673479f6b030e14123571816e79312c0466a0
welthulk <g1403.2019@udoschmitz.de>
2024-07-02 15:27:45 +0200

    added test_ABCD testfunction

--- test/testABCD.jl
+++ test/testABCD.jl
@@ -4,24 +4,61 @@ using Logging
 
 
 function test_ABCD(verbose::Int = 0)
+  function run_acpflow(net::Net, verbose::Int = 0)
+    
+    if verbose < 1
+      return
+    end
+
+    tol = 1e-6
+    maxIte = 10
   
-  # Netzwerk erstellen
-  Sbase_MVA = 100.0  # Basis-Scheinleistung in MVA
-  netName = "testnet"
-  net = Net(name=netName, baseMVA=Sbase_MVA)
+    result = true
+    etime = @elapsed begin
+      ite, erg = runpf!(net, maxIte, tol, verbose)
+    end
+    if erg != 0
+      @warn "test_ABCD: Power flow did not converge"
+      result = false
+    end
 
-  # Busse hinzufügen
-  addBus!(net = net, busName = "B1", busType = "Slack", vn_kV = 220.0)  
-  addBus!(net= net,  busName=  "B2", busType = "PQ", vn_kV = 220.0)
+    if verbose > 0
+      calcNetLosses!(net)
+      printACPFlowResults(net, etime, ite, tol)
+    end
+  end
 
-  addACLine!(net = net, fromBus = "B1", toBus = "B2", length = 100.0, r = 0.0653, x = 0.398, c_nf_per_km = 9.08, tanδ = 0.0)
-  
-  addProsumer!(net = net, busName = "B1", type = "EXTERNALNETWORKINJECTION", vm_pu = 1.0, va_deg = 0.0, referencePri = "B1")
-  addProsumer!(net = net, busName = "B2", type = "ENERGYCONSUMER", p = 200.0, q = 10.0)
+  function createNet(mode)
+    Sbase_MVA = 100.0  # Basis-Scheinleistung in MVA
+    netName = "testnet-ABCD"
+    net = Net(name=netName, baseMVA=Sbase_MVA)  
+    if mode == "line"
+     vn_kV_b1 = 220.0
+     vn_kV_b2 = 220.0
+    elseif mode =="2WT"
+      vn_kV_b1 = 110.0
+      vn_kV_b2 = 10.0
+    else
+     @error "Unknown mode: $mode"
+     return
+    end
+    # Busse hinzufügen
+    addBus!(net = net, busName = "B1", busType = "Slack", vn_kV = vn_kV_b1)  
+    addBus!(net= net,  busName=  "B2", busType = "PQ", vn_kV = vn_kV_b2)
+    if mode == "line"
+      addACLine!(net = net, fromBus = "B1", toBus = "B2", length = 100.0, r = 0.0653, x = 0.398, c_nf_per_km = 9.08, tanδ = 0.0)
+    elseif mode =="2WT"
+      add2WTrafo!(net=net, fromBus="B1", toBus="B2", sn_mva = Sbase_MVA, vk_percent = 10.0, vkr_percent = 0.1, pfe_kw = 0.0, i0_percent = 0.0)
+    end  
+    addProsumer!(net = net, busName = "B1", type = "EXTERNALNETWORKINJECTION", vm_pu = 1.0, va_deg = 0.0, referencePri = "B1")
+    addProsumer!(net = net, busName = "B2", type = "ENERGYCONSUMER", p = 200.0, q = 10.0)
 
-  # Transformator hinzufügen
-  #add2WTrafo!(net=net, fromBus="Bus1", toBus="Bus2", vnom_from_kV=110.0, vnom_to_kV=10.0, snom_MVA=50.0, vnom_percent=10.0, tap=1.0)
 
+    return net
+  end
+  result = true  
+  # Netzwerk erstellen  
+  net = createNet("line")
   # YBus-Matrix berechnen
   Y_bus_1 = createYBUS(net=net, sparse=false, printYBUS=(verbose > 0))
   Y_bus_2 = createYBUS_ABCD(net=net, sparse=false, printYBUS=(verbose > 0))
@@ -30,11 +67,26 @@ function test_ABCD(verbose::Int = 0)
   if verbose > 0
     @show br
   end
-  if isapprox(Y_bus_1, Y_bus_2, atol=1e-6)
-    return true
-  else
-    return false
+  if !isapprox(Y_bus_1, Y_bus_2, atol=1e-6)
+    result =  false
+  end
+  
+  run_acpflow(net, verbose)
+
+  netName = "testnet-trafo"
+  net = createNet("2WT")  
+  Y_bus_1 = createYBUS(net=net, sparse=false, printYBUS=(verbose > 0))
+  Y_bus_2 = createYBUS_ABCD(net=net, sparse=false, printYBUS=(verbose > 0))
+  br = getNetBranch(net = net, fromBus = "B1", toBus = "B2")
+  if verbose > 0
+    @show br
+  end
+  if !isapprox(Y_bus_1, Y_bus_2, atol=1e-6)
+    result =  false
   end
 
+  run_acpflow(net, verbose)
+
+  return result
 end
 
