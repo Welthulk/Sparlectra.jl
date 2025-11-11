615ce73cba5ab3537c3c58ad3b65f924af971889
welthulk <g1403.2019@udoschmitz.de>
2025-11-11 15:54:33 +0100

    translations

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,16 +11,16 @@ include("testremove.jl")
 include("test_jacobian_full.jl")
 
 @testset "Sparlectra.jl" begin
-    #@test testNetwork() == true
-    #@test test_NBI_MDO() == true
-    #@test test_acpflow(0) == true
-    #@test testISOBusses() == true
-    #@test testRemoveFunctions() == true
+    @test testNetwork() == true
+    @test test_NBI_MDO() == true
+    @test test_acpflow(0) == true
+    @test testISOBusses() == true
+    @test testRemoveFunctions() == true
     # @test testImportMatpower() == true  # needs file in repo    
     @testset "Full-system NR (PV identity rows)" begin
-        #@test test_acpflow_full(0) == true
-        #@test test_full_matches_reduced() == true
-        #@test test_jacobian_full_structure(verbose=1) == true
-        @test test_pv_q_limit_switch(verbose=1) == true
+        @test test_acpflow_full(0) == true
+        @test test_full_matches_reduced() == true
+        @test test_jacobian_full_structure() == true
+        @test test_pv_q_limit_switch(verbose=0) == true
     end
 end
