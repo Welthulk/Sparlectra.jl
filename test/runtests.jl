49ade3b5cd5887cd216eab42655f55e213e46309
welthulk <g1403.2019@udoschmitz.de>
2025-11-27 16:48:02 +0100

    small

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,4 +23,23 @@ include("test_jacobian_full.jl")
         @test test_5BusNet(0,15.0) == true
         @test test_5BusNet(0,30.0) == false
     end
+    @testset "Rectangular complex NR vs. runpf_full!" begin
+        net1 = createTest5BusNet(pq_only=false)
+        net2 = createTest5BusNet(pq_only=false)
+
+        # Klassischer Solver
+        it_polar, erg_polar = runpf_full!(net1, 20, 1e-8, 0)
+        @test erg_polar == 0
+
+        # Rechteck-Solver
+        it_rect, erg_rect = run_complex_nr_rectangular_for_net!(net2; maxiter=20, tol=1e-6, damp=1.0, verbose=0)
+        @test erg_rect == 0
+
+        V_polar = [n._vm_pu * cis(deg2rad(n._va_deg)) for n in net1.nodeVec]
+        V_rect  = [n._vm_pu * cis(deg2rad(n._va_deg)) for n in net2.nodeVec]
+
+        @test maximum(abs.(abs.(V_polar) .- abs.(V_rect))) < 1e-4
+        @test maximum(abs.(angle.(V_polar) .- angle.(V_rect))) < 1e-3
+    end
+
 end
