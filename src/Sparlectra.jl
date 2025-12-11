82a5a0b1c71bd743250b0ec64764dc6630958c05
welthulk <g1403.2019@udoschmitz.de>
2025-12-11 16:24:48 +0100

    version

--- src/Sparlectra.jl
+++ src/Sparlectra.jl
@@ -13,7 +13,7 @@ using LinearAlgebra, Dates, SparseArrays, Printf, Logging
 
 # resource data types for working with Sparlectra
 const Wurzel3 = 1.7320508075688772
-const SparlectraVersion = v"0.4.26"
+const SparlectraVersion = v"0.4.27"
 version() = SparlectraVersion
 abstract type AbstractBranch end
 
